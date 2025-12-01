cf01e9bda24d37f66608a59b8b874146a83c7def
AuraSRamirez <aura88060@gmail.com>
2025-11-30 18:51:36 -0600

    Creación del Diccionario y Estadísticas (Diccionario.cpp)

--- ProyectoIntroduccionCompu.cpp
+++ ProyectoIntroduccionCompu.cpp
@@ -38,3 +38,102 @@ void	Diccionario(char* szNombre, char szPalabras[NUMPALABRAS][TAMTOKEN], int iEs
 	fopen_s(&stream, szNombre, "r");
 	if (stream == NULL)
 		exit(0);
+
+    iNumElementos = 0; j = 0;
+    szCaracter = fgetc(stream);
+    while (feof(stream) == 0)
+    {
+        if (!((szCaracter == 9) || (szCaracter == 10) || (szCaracter == 13) || (szCaracter == 32) || (szCaracter == 41) || (szCaracter == 44) || (szCaracter == 46) || (szCaracter == 59) || (szCaracter == 40)))
+        {
+            szRecibePalabras[iNumElementos][j] = tolower((char)szCaracter);
+            szCaracter = fgetc(stream);
+            j++;
+
+        }
+        else
+        {
+            if (j > 0)
+            {
+                szRecibePalabras[iNumElementos][j] = '\0';
+                iEstadisticas[iNumElementos] = 1;
+
+                if (iNumElementos > 0)
+                {
+                    for (i = 0; i < iNumElementos; i++)
+                        if (strcmp(szRecibePalabras[i], szRecibePalabras[iNumElementos]) == 0)
+                        {
+                            iEstadisticas[i] = iEstadisticas[i] + 1;
+                            i = iNumElementos;
+                            iNumElementos = iNumElementos - 1;
+                        }
+                }
+
+                iNumElementos++;
+                j = 0;
+            }
+            szCaracter = fgetc(stream);
+        }
+    }
+    fclose(stream);
+
+    szRecibePalabras[iNumElementos][j] = '\0';
+    iEstadisticas[iNumElementos] = 1;
+
+    for (i = 0; i < iNumElementos; i++)
+        if (strcmp(szRecibePalabras[i], szRecibePalabras[iNumElementos]) == 0)
+        {
+            iEstadisticas[i] = iEstadisticas[i] + 1;
+            strcpy_s(szRecibePalabras[iNumElementos], "");
+            i = iNumElementos;
+        }
+
+    for (i = 0; i < iNumElementos; i++)
+    {
+        iSegurofin = 0;
+        for (j = 0; j < iNumElementos; j++)
+        {
+            if (strcmp(szRecibePalabras[j + 1], szRecibePalabras[j]) < 0)
+            {
+                strcpy_s(szControl, szRecibePalabras[j]);
+                iControl = iEstadisticas[j];
+                strcpy_s(szRecibePalabras[j], szRecibePalabras[j + 1]);
+                iEstadisticas[j] = iEstadisticas[j + 1];
+                strcpy_s(szRecibePalabras[j + 1], szControl);
+                iEstadisticas[j + 1] = iControl;
+            }
+            else
+                iSegurofin += 1;
+        }
+        if (iSegurofin == iNumElementos)
+            i = iNumElementos;
+    }
+
+    if (strcmp(szRecibePalabras[0], "") == 0)
+    {
+        for (i = 0; i < iNumElementos; i++)
+        {
+            strcpy_s(szPalabras[i], szRecibePalabras[i + 1]);
+            iEstadisticas[i] = iEstadisticas[i + 1];
+        }
+        iEstadisticas[i] = '\0';
+    }
+    else
+    {
+        iNumElementos++;
+        for (i = 0; i < iNumElementos; i++)
+            strcpy_s(szPalabras[i], szRecibePalabras[i]);
+    }
+    /*
+        printf("Elementos en el diccionario: %i", iNumElementos);
+
+        fopen_s(&stream, "datos.csv", "w");
+        if (stream == NULL)
+            exit(0);
+
+        for(i=0;i<iNumElementos;i++)
+            fprintf(stream,"\n%s, %i", szPalabras[i],iEstadisticas[i]);
+
+        fclose(stream);
+    */
+    //Sustituya estas lineas por su código
+    /*
