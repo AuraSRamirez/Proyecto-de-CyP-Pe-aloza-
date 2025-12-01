1b265a733ab197c6c68a6aa2189992d29ef26961
AuraSRamirez <aura88060@gmail.com>
2025-11-30 18:48:24 -0600

    Archivos de Configuración e Inclusión (Config & Headers) Creación del Diccionario y Estadísticas (Diccionario.cpp)

--- ProyectoIntroduccionCompu.cpp
+++ ProyectoIntroduccionCompu.cpp
@@ -1,19 +1,40 @@
 // ProyectoIntroduccionCompu.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+/*****************************************************************************************************************
+	UNIVERSIDAD NACIONAL AUTONOMA DE MEXICO
+	FACULTAD DE ESTUDIOS SUPERIORES -ARAGON-
 
-#include <iostream>
+	Computadoras y programacion.
+	(c) Ponga su nombre y numero de cuenta aqui.
 
-int main()
+	Quiso decir: Programa principal de la aplicacion de la distancia de Levenstein.
+
+******************************************************************************************************************/
+
+
+#include "stdafx.h"
+#include <string.h>
+#include "corrector.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define ALFABETO 32
+
+
+//Funciones publicas del proyecto
+/*****************************************************************************************************************
+	DICCIONARIO: Esta funcion crea el diccionario completo
+	char *	szNombre				:	Nombre del archivo de donde se sacaran las palabras del diccionario
+	char	szPalabras[][TAMTOKEN]	:	Arreglo con las palabras completas del diccionario
+	int		iEstadisticas[]			:	Arreglo con el numero de veces que aparecen las palabras en el diccionario
+	int &	iNumElementos			:	Numero de elementos en el diccionario
+******************************************************************************************************************/
+void	Diccionario(char* szNombre, char szPalabras[NUMPALABRAS][TAMTOKEN], int iEstadisticas[NUMPALABRAS], int& iNumElementos)
 {
-    std::cout << "Hello World!\n";
-}
+	FILE* stream;
+	char szRecibePalabras[NUMPALABRAS][TAMTOKEN];
+	char szControl[TAMTOKEN];
+	int  szCaracter, i, j, iSegurofin, iControl;
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+	fopen_s(&stream, szNombre, "r");
+	if (stream == NULL)
+		exit(0);
